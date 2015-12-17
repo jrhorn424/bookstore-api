60317e5c42375a8f76cfacb652eec5d57a9c1968
Jeffrey Horn <jeffh@generalassemb.ly>
2015-12-16 20:21:40 -0500

    Make resource URL compatible with JSON API standard

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :publishing_houses
+  resources :publishing_houses, path: 'publishing-houses'
   resources :authors
   resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
